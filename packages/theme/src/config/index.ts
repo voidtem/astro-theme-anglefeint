82d83b503ebdbe021ca5220efa58c7ba39cd7184
Voidtemple <xukongyoulong@gmail.com>
2026-02-28 00:27:53 +0800

    chore(starter): switch starter to npm package dependency (@anglefeint/astro-theme)

--- packages/theme/src/config/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Central config. Re-export all config modules for convenience.
- */
-export * from './site';
-export * from './social';
-export * from './theme';
-export * from './about';
--- packages/theme/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './consts';
-export * from './i18n/config';
-export * from './i18n/posts';
-export { getMessages } from './i18n/messages';
-export { collections } from './content-schema';
